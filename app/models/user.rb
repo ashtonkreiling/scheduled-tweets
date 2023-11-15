c0b2e960422c40e25df223995da34a2b4ad13a07
ashtonkreiling <ashtonkreiling@gmail.com>
2023-11-14 21:19:21 -0700

    Make Users Model enforce email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@
 # Virtual methods added by has_secure_password
 class User < ApplicationRecord
     has_secure_password
+
+    # Validate that the email field is not null
+    validates :email, presence: true
 end
--- db/migrate/20231115040127_create_users.rb
+++ db/migrate/20231115040127_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email
+      # DB Validates email is not null
+      t.string :email, null: false
       t.string :password_digest
 
       t.timestamps
